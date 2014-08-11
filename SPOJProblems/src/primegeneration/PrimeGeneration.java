77793fd7268a034823c79c923afc0a693ba432d0
opnet <opnet.leet.la@gmail.com>
2014-08-11 11:45:27 -0700

    Some obvious problems in the earlier version fixed.

--- SPOJProblems/src/primegeneration/PrimeGeneration.java
+++ SPOJProblems/src/primegeneration/PrimeGeneration.java
@@ -1,17 +1,26 @@
 package primegeneration;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Find all prime numbers p between two numbers n and m, where n <= p <= m
  */
 
 public class PrimeGeneration
-{
-	public static void main(String[] args)
+{	
+	public static void main(String[] args) throws NumberFormatException, IOException
 	{
-		boolean[] notPrime = new boolean[1000000000];
+		int sqrLimit = (int)Math.sqrt(1000000000);
+		boolean[] found = new boolean[1000000000];
 		
-		for(int i = 3; i < notPrime.length; i += 2)
-			for(int j = i + i; j < j * j && j < notPrime.length; j += i)
-				notPrime[j] = true;
+		for(int i = 3; i < sqrLimit; i += 2)
+		{
+			if(found[i])
+				continue;
+			
+			for(int j = i + i; j < found.length; j += i)
+				found[j] = true;
+		}
 	}
 }
